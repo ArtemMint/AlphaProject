8bbd6352ea34d85f31129da2c162e05bcaae6826
Artem Yurchak <artem.yurchak@xperi.com>
2022-05-18 21:20:04 +0300

    Added login and logout feature

--- apps/register/forms/register.py
+++ apps/register/forms/register.py
@@ -4,26 +4,31 @@ from _db import models
 
 
 class RegisterUserForm(forms.ModelForm):
-
     class Meta:
         model = models.User
         fields = (
+            # 'user_name',
             'email',
             'password',
-            # 'password2',
+            # 'confirm',
         )
         widgets = {
+            # 'user_name': forms.EmailInput(
+            #     attrs={
+            #         'class': 'form-control',
+            #     }
+            # ),
             'email': forms.EmailInput(
                 attrs={
                     'class': 'form-control',
                 }
             ),
-            'password1': forms.PasswordInput(
+            'password': forms.PasswordInput(
                 attrs={
                     'class': 'form-control',
                 }
             ),
-            # 'password2': forms.PasswordInput(
+            # 'confirm': forms.PasswordInput(
             #     attrs={
             #         'class': 'form-control',
             #     }
--- apps/register/views.py
+++ apps/register/views.py
@@ -1,63 +1,56 @@
+from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 from _db import models
+from register import forms
 
 
-class LoginUserView(views.LoginView):
-    """Login user in site"""
+class LoginUserView(generic.View):
     template_name = 'register/login.html'
-    # redirect_authenticated_user = True
+    login_redirect = 'website:home_page'
 
     def get(self, request):
         return render(
-            self.request,
-            self.template_name
+            request,
+            self.template_name,
+            {}
         )
 
     def post(self, request):
-        email = request.POST['email']
-        password = request.POST['password']
+        email = request.POST.get('email')
+        password = request.POST.get('password')
         user = authenticate(
-            email=email,
+            request,
+            username=email,
             password=password,
         )
-        context = {}
         if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect(
-                    self.request.GET.get('next', 'register:user_login'),
-                )
-            else:
-                context['error_message'] = "User is not active."
-        else:
-            context['error_message'] = "Email or password not correct."
-
+            login(request, user)
+            return redirect(self.login_redirect)
         return render(
-            self.request,
+            request,
             self.template_name,
-            context,
-        )
-
-
-class RegisterUserView(generic.View):
-    """Register new user view"""
-    model = models.User
-    template_name = 'register/register.html'
-
-    def get(self, request):
-        return render(
-            self.request,
-            self.template_name,
+            {}
         )
 
 
 class LogoutUserView(generic.View):
-    template_name = 'register:user_login'
-    """Logout current user"""
+    logout_redirect = 'website:home_page'
+
     def get(self, request):
         logout(request)
-        return redirect(self.template_name)
+        return redirect(self.logout_redirect)
+
+
+
+
+class RegisterUserView(generic.CreateView):
+    """Register new user view"""
+    form_class = forms.RegisterUserForm
+    success_url = reverse_lazy('sign-in/')
+    template_name = 'register/register.html'
+    success_message = "Your profile was created successfully!"
+
